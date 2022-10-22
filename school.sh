9c338b4d0d1c28b3aba8a6de6b494bd4f9a1b19c
Jiayang Wang <jwang2657@wisc.edu>
2022-10-21 19:22:06 -0500

    Add school.sh.

---
+++ school.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+head Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d, -f 7 | { sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$(($count + 1)); done; echo "sum=$sum; count=$count; average = $(($sum/$count))"; }
